99fcc97307c32bcb9a93daae3300a41a9c30eb7d
AmeliePick <ameliepickdev@gmail.com>
2025-07-03 23:22:34 +0300

    feature(src/launcher.cpp) Add dll launcher.

---
+++ src/launcher.cpp
@@ -0,0 +1,20 @@
+﻿#include <windows.h>
+
+typedef void(*Fucn)();
+
+int main()
+{
+    ShowWindow(GetConsoleWindow(), SW_HIDE);
+
+    HMODULE hLib = LoadLibrary("Dll1.dll");
+    if (!hLib) return 1;
+
+    Fucn dllMain = (Fucn)GetProcAddress(hLib, "mainOwO");
+    if (!dllMain)
+    {
+        FreeLibrary(hLib);
+        return 1;
+    }
+
+    dllMain();
+}
