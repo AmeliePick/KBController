2c6b672ddb8a1307c81b67f8bedba4902bc368c5
AmeliePick <ameliepickdev@gmail.com>
2025-08-18 23:43:49 +0300

    feature(src/Backlight) Add full key-map and backlight control.

---
+++ src/Backlight.cpp
@@ -0,0 +1,96 @@
+#include "Backlight.h"
+#include <cstring>
+#include <algorithm>
+
+
+extern uint8 keyboardData[520];
+
+
+Backlight::Color&& Backlight::Color::Create(uint8 r, uint8 g, uint8 b, uint8 brightness)
+{
+    // By default, the official software just changes the color values, not the physical brightness levels of LEDS.
+    // For example, if color is 255; 255; 255 and "Visibility" set to 50 then the color isn't pure white. The pure white will be only
+    // when "Visibility" is 100.
+
+    return Color{};
+}
+
+
+
+void Backlight::SetKey(Keys key, Color color)
+{
+    *(keyboardData + (  8 + (uint8)key)) = color.r;
+    *(keyboardData + (134 + (uint8)key)) = color.g;
+    *(keyboardData + (260 + (uint8)key)) = color.b;
+}
+
+
+void Backlight::SetRow(__int8 row, Color color)
+{
+    switch (row)
+    {
+    case 1:
+        memset(keyboardData +   8, color.r, 19);
+        memset(keyboardData + 134, color.g, 19);
+        memset(keyboardData + 260, color.b, 19);
+        break;
+    case 2:
+        memset(keyboardData +   8 + 21, color.r, 14); memset(keyboardData +   8 + 36, color.r, 4);
+        memset(keyboardData + 134 + 21, color.g, 14); memset(keyboardData + 134 + 36, color.g, 4);
+        memset(keyboardData + 260 + 21, color.b, 19); memset(keyboardData + 260 + 36, color.b, 4);
+        break;
+    case 3:
+        memset(keyboardData +   8 + 42, color.r, 14); memset(keyboardData +   8 + 57, color.r, 4);
+        memset(keyboardData + 134 + 42, color.g, 14); memset(keyboardData + 134 + 57, color.g, 4);
+        memset(keyboardData + 260 + 42, color.b, 14); memset(keyboardData + 260 + 57, color.b, 4);
+        break;
+    case 4:
+        memset(keyboardData +   8 + 63, color.r, 12); memset(keyboardData +   8 + 78, color.r, 3);
+        memset(keyboardData + 134 + 63, color.g, 12); memset(keyboardData + 134 + 78, color.g, 3);
+        memset(keyboardData + 260 + 63, color.b, 12); memset(keyboardData + 260 + 78, color.b, 3);
+        SetKey(Backlight::Keys::Enter, color);
+        break;
+    case 5:
+        memset(keyboardData +   8 + 86, color.r, 12); memset(keyboardData +   8 + 99, color.r, 4);
+        memset(keyboardData + 134 + 86, color.g, 12); memset(keyboardData + 134 + 99, color.g, 4);
+        memset(keyboardData + 260 + 86, color.b, 12); memset(keyboardData + 260 + 99, color.b, 4);
+        SetKey(Backlight::Keys::Shift, color);
+        break;
+    case 6:
+        memset(keyboardData +   8 + 105, color.r, 3); memset(keyboardData +   8 + 120, color.r, 3);
+        memset(keyboardData + 134 + 105, color.g, 3); memset(keyboardData + 134 + 120, color.g, 3);
+        memset(keyboardData + 260 + 105, color.b, 3); memset(keyboardData + 260 + 120, color.b, 3);
+        SetKey(Backlight::Keys::Space, color); SetKey(Backlight::Keys::RAlt,  color);
+        SetKey(Backlight::Keys::Fn,    color); SetKey(Backlight::Keys::RCtrl, color);
+        SetKey(Backlight::Keys::LEFT,  color); SetKey(Backlight::Keys::DOWN,  color);
+        break;
+    default:
+        break;
+    }
+}
+
+
+void Backlight::SetNumPad(Color color)
+{
+    memset(keyboardData +   8 + 36,  color.r, 4);
+    memset(keyboardData + 134 + 36,  color.b, 4);
+    memset(keyboardData + 260 + 36,  color.b, 4);
+
+    memset(keyboardData +   8 + 57, color.r, 4);
+    memset(keyboardData + 134 + 57, color.b, 4);
+    memset(keyboardData + 260 + 57, color.b, 4);
+
+    memset(keyboardData +   8 + 78, color.r, 3);
+    memset(keyboardData + 134 + 78, color.b, 3);
+    memset(keyboardData + 260 + 78, color.b, 3);
+
+    memset(keyboardData +   8 + 99, color.r, 4);
+    memset(keyboardData + 134 + 99, color.b, 4);
+    memset(keyboardData + 260 + 99, color.b, 4);
+
+    memset(keyboardData +   8 + 121, color.r, 2);
+    memset(keyboardData + 134 + 121, color.b, 2);
+    memset(keyboardData + 260 + 121, color.b, 2);
+}
+
+
---
+++ src/Backlight.h
@@ -0,0 +1,34 @@
+#pragma once
+
+typedef unsigned __int8 uint8;
+
+namespace Backlight
+{
+    enum class Keys : uint8
+    {
+        ESC, F1, F2, F3, F4, F5, F6, F7, F8, F9, F10, F11, F12, PrtSc, Del, Home, Ins, PgUp, PgDn,
+        Tilde = 21, _1, _2, _3, _4, _5, _6, _7, _8, _9, _0, Minus, Plus, Backspace, NumLock = 36, NumSlash, NumMul, NumMinus,
+        Tab = 42, Q, W, E, R, T, Y, U, I, O, P, LeftBrace, RightBrace, Backslash, Num7 = 57, Num8, Num9, NumPlus,
+        CapsLock = 63, A, S, D, F, G, H, J, K, L, Semiñolon, DoubleQuotes, Enter = 76, Num4 = 78, Num5, Num6,
+        Shift = 84, Z = 86, X, C, V, B, N, M, Comma, Period, Slash, RShift, UP, Num1 = 99, Num2, Num3, NumEnter,
+        Ctrl = 105, Win, Alt, Space = 110, RAlt = 113, Fn, RCtrl, LEFT = 117, DOWN, RIGHT = 120, Num0, NumPeriod
+    };
+
+    struct Color
+    {
+        uint8 r;
+        uint8 g;
+        uint8 b;
+
+        static Color&& Create(uint8 r, uint8 g, uint8 b, uint8 brightness);
+    };
+
+    // -- Sets the color for the key --
+    // Use black color to turn off the key.
+    void SetKey(Keys key, Color color);
+
+    // -- Sets the whole row to the color --
+    void SetRow(__int8 row, Color color);
+
+    void SetNumPad(Color color);
+}
